b1e356d377a9d5f9d8db5ee8ae2dd8e65d953070
U-SCDOM\ADR <ADR@US01WP0581.scdom.net>
2020-04-06 14:47:43 -0400

    add SELECT_SOFT_SERIAL_SPEED

--- keyboards/ADRredox/rules.mk
+++ keyboards/ADRredox/rules.mk
@@ -29,7 +29,7 @@ UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
-RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight. 
+RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight. 
 EXTRAFLAGS += -flto			# Added by ADR to optimize space
 SPLIT_KEYBOARD = yes
 
